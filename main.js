71c499e6afb54c36abcf42351de7ef23eb02cde7
alexeyAxyonov <alexey.axyonoy@gmail.com>
2026-02-18 10:52:49 +0700

    Создана функция создания переменной
    Также проработана логика хранения переменных и массивов в памяти.

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
 
-function Addition(var1, var2){
-    return var1 + var2;
-}
+let variables_and_arrays = {};
 
-class StartBlock{
-    past_block = null;
-    next_block = null;
-
-    add_next_block(id){
-        this.next_block = id;
-    }
-    remove_next_block(){
-        //TODO: сделать эту функцию
-    }
-}
-class EndBlock{
-    past_block = null;
-    next_block = null;
-    add_next_block(id){
-        this.next_block = id;
-    }
-    remove_next_block(){
-        //TODO: сделать эту функцию
-    }
-}
-
-class TestBlock{
+class BaseBlock{
+    //Если хотите изменить поведение всех блоков в целом, то изменяйте этот класс. От него будут наследоваться все остальные блоки
     past_block = null;
     next_block = null
     id = null;
@@ -46,6 +23,56 @@ class TestBlock{
     }
     //TODO: реализовать drag-and-drop
 }
+
+class ArithmeticOperationBlock{
+
+}
+
+class StartBlock extends BaseBlock{
+}
+
+class EndBlock extends BaseBlock{
+}
+
+class Equals extends ArithmeticOperationBlock{
+
+}
+
+class VariableBlock extends BaseBlock{
+    /*Класс блоков с переменными. При наведении отображает значение.*/
+}
+
+function AddVariables(variables){
+    //Получает список переменных из variables
+    function is_valid_variable_name(str_var){
+        const forbidden_characters = [
+        'break', 'case', 'catch', 'class', 'const', 'continue', 'debugger',
+        'default', 'delete', 'do', 'else', 'export', 'extends', 'finally',
+        'for', 'function', 'if', 'import', 'in', 'instanceof', 'new',
+        'return', 'super', 'switch', 'this', 'throw', 'try', 'typeof',
+        'var', 'void', 'while', 'with', 'yield',
+        'let', 'static', 'implements', 'interface', 'package', 'private',
+        'protected', 'public', 'await'];
+        
+        if (forbidden_characters.includes(str_var)){
+            return false;
+        }
+        const regex = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/
+        return regex.test(str_var)
+    }
+    let intermediate_value = variables.split(/\s*,\s*/).filter(item => item !== '');
+    for (let i = 0; i < intermediate_value.length; i++){
+        if (is_valid_variable_name(intermediate_value[i])){
+            variables_and_arrays[intermediate_value[i]] = 0;
+            //TODO: добавить объекты VariableBlock.
+        }
+        else{
+            //TODO: возможно вывести ошибку? или просто проигнорить и записать только
+            //валидные названия.
+        }
+    }
+}
+
 
 function StartProgram(start_id){
     start_id.next_block = id
